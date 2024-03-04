3572fd6d0ab501734786af2df60376ef3fa8a5f8
bombatop <ella.katzenellennbogen@gmail.com>
2024-03-04 22:37:24 +0800

    it fucking works mate

--- springboot-backend/src/main/java/courseproject/springbootbackend/SpringbootBackendApplication.java
+++ springboot-backend/src/main/java/courseproject/springbootbackend/SpringbootBackendApplication.java
@@ -1,13 +1,13 @@
-package demo;
+package courseproject.springbootbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SpringbootBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SpringbootBackendApplication.class, args);
 	}
 
 }
